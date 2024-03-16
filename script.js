9785c7e2e085d2458e70c160337fe78665670a08
Ashwin Anil <ashanil005@gmail.com>
2024-03-16 14:50:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "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";
+const apiKey = "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";
 
 const maxImages = 4; // Number of images to generate for each prompt
 let selectedImageNumber = null;
